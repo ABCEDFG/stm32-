f3fab4e52e53a5e2429e35fe79cae93bc60c53a0
千里客 <2812929818@qq.com>
2023-09-18 01:33:55 +0800

    给三个 0 xxT6工程添加了可变参的调试宏

--- 0  C8T6工程/Public/usart.h
+++ 0  C8T6工程/Public/usart.h
@@ -5,6 +5,14 @@
 #include "stdio.h" 
 
 
+#define DEBUG_FLAG
+#ifdef  DEBUG_FLAG
+#define DEBBUG_PRINT(x, ...)  printf(x, ##__VA_ARGS__)
+#else
+#define DEBBUG_PRINT(x, ...)
+#endif
+
+
 void Usart1_Init(u32 bound);
 
 
